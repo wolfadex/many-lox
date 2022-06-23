56cc9f3ba33cafa3ec174d88ddcd17f8748575d6
wolfadex <wolfadex@gmail.com>
2022-06-23 18:52:30 -0500

    Basic haskell argument passing/reading

--- haskell-lox/app/Main.hs
+++ haskell-lox/app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
-import Lib
+import qualified System.Exit
+import System.Exit (ExitCode(..))
+import qualified System.Environment
+import qualified System.IO
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- System.Environment.getArgs
+  case args of
+    [] ->
+      runPrompt
+
+    [filepath] -> 
+      runFile filepath
+    _ -> do
+      putStrLn "Usage: haskell-lox <script>"
+      System.Exit.exitWith (ExitFailure 64)
+
+
+runPrompt :: IO ()
+runPrompt =
+  undefined
+
+
+runFile :: FilePath -> IO ()
+runFile filepath = do
+  contents <- System.IO.readFile filepath
+  putStrLn contents
+  
