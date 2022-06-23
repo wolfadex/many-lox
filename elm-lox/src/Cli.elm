26a5bd82523e11bf7f0771657762c0dd36ccadd6
wolfadex <wolfadex@gmail.com>
2022-06-22 19:39:49 -0500

    Add support for nicer error messages

--- elm-lox/src/Cli.elm
+++ elm-lox/src/Cli.elm
@@ -7,18 +7,51 @@ import Posix.IO.Process as Process
 
 program : Process -> IO ()
 program process =
-    case process.argv of
+    case Debug.log "args" process.argv of
         [ _, filename ] ->
             File.contentsOf filename
                 |> IO.exitOnError identity
-                |> IO.andThen (Debug.todo "runFile")
+                |> IO.map run
+                |> IO.exitOnError reportError
+                |> IO.andThen Process.print
 
         [ _ ] ->
-            repl
+            -- repl
+            Process.print "repl"
 
         _ ->
             Process.logErr "Usage: elm-lox <file>\n"
                 |> IO.andThen (\() -> Process.exit 64)
+
+
+run : String -> Result Error String
+run source =
+    scanTokens source
+        |> List.map identity
+        |> String.join "\n"
+        |> Ok
+
+
+scanTokens : String -> List String
+scanTokens source =
+    Debug.todo "tokenize!"
+
+
+type Error
+    = Error
+        { line : Int
+        , message : String
+        }
+
+
+error : { line : Int, message : String } -> Error
+error err =
+    Error { line = err.line, message = err.message }
+
+
+reportError : Error -> String
+reportError (Error err) =
+    "[line " ++ String.fromInt err.line ++ "] Error: " ++ err.message
 
 
 repl =
