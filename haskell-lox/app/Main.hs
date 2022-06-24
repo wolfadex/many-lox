fca97d41c0b1fa4f178512cc10465eb6afc6b977
wolfadex <wolfadex@gmail.com>
2022-06-23 19:39:13 -0500

    Haskell token setup complete

--- haskell-lox/app/Main.hs
+++ haskell-lox/app/Main.hs
@@ -70,14 +70,112 @@ errorToString err =
   "[line " <> (show $ errLine err) <> "] Error: " <> errMessage err
 
 
-data Token = Token String
+
+data TokenType
+    -- Single-character tokens
+    = LEFT_PAREN
+    | RIGHT_PAREN
+    | LEFT_BRACE
+    | RIGHT_BRACE
+    | COMMA
+    | DOT
+    | MINUS
+    | PLUS
+    | SEMICOLON
+    | SLASH
+    | STAR
+    -- One or two character tokens
+    | BANG
+    | BANG_EQUAL
+    | EQUAL
+    | EQUAL_EQUAL
+    | GREATER
+    | GREATER_EQUAL
+    | LESS
+    | LESS_EQUAL
+    -- Literals
+    | IDENTIFIER String
+    | STRING String
+    | NUMBER Float
+    -- Keywords
+    | AND
+    | CLASS
+    | ELSE
+    | FALSE
+    | FUN
+    | FOR
+    | IF
+    | NIL
+    | OR
+    | PRINT
+    | RETURN
+    | SUPER
+    | THIS
+    | TRUE
+    | VAR
+    | WHILE
+    | EOF
+
+
+tokenTypeToString :: TokenType -> ( String, Maybe String )
+tokenTypeToString tokenType =
+  case tokenType of
+    LEFT_PAREN -> ( "LEFT_PAREN", Nothing )
+    RIGHT_PAREN -> ( "RIGHT_PAREN", Nothing )
+    LEFT_BRACE -> ( "LEFT_BRACE", Nothing )
+    RIGHT_BRACE -> ( "RIGHT_BRACE", Nothing )
+    COMMA -> ( "COMMA", Nothing )
+    DOT -> ( "DOT", Nothing )
+    MINUS -> ( "MINUS", Nothing )
+    PLUS -> ( "PLUS", Nothing )
+    SEMICOLON -> ( "SEMICOLON", Nothing )
+    SLASH -> ( "SLASH", Nothing )
+    STAR -> ( "STAR", Nothing )
+    BANG -> ( "BANG", Nothing )
+    BANG_EQUAL -> ( "BANG_EQUAL", Nothing )
+    EQUAL -> ( "EQUAL", Nothing )
+    EQUAL_EQUAL -> ( "EQUAL_EQUAL", Nothing )
+    GREATER -> ( "GREATER", Nothing )
+    GREATER_EQUAL -> ( "GREATER_EQUAL", Nothing )
+    LESS -> ( "LESS", Nothing )
+    LESS_EQUAL -> ( "LESS_EQUAL", Nothing )
+    IDENTIFIER literal -> ( "IDENTIFIER", Just literal )
+    STRING literal -> ( "STRING", Just literal )
+    NUMBER num -> ( "NUMBER", Just (show num) )
+    AND -> ( "AND", Nothing )
+    CLASS -> ( "CLASS", Nothing )
+    ELSE -> ( "ELSE", Nothing )
+    FALSE -> ( "FALSE", Nothing )
+    FUN -> ( "FUN", Nothing )
+    FOR -> ( "FOR", Nothing )
+    IF -> ( "IF", Nothing )
+    NIL -> ( "NIL", Nothing )
+    OR -> ( "OR", Nothing )
+    PRINT -> ( "PRINT", Nothing )
+    RETURN -> ( "RETURN", Nothing )
+    SUPER -> ( "SUPER", Nothing )
+    THIS -> ( "THIS", Nothing )
+    TRUE -> ( "TRUE", Nothing )
+    VAR -> ( "VAR", Nothing )
+    WHILE -> ( "WHILE", Nothing )
+    EOF -> ( "EOF", Nothing )
+
+
+
+data Token = Token
+  { tokType :: TokenType
+  , tokLine :: Int
+  , tokLexeme :: String
+  }
 
 
 tokenToString :: Token -> String
-tokenToString (Token token) =
-  token
+tokenToString token =
+  case tokenTypeToString (tokType token) of
+    ( type_, Nothing ) -> type_ <> " " <> tokLexeme token
+    ( type_, Just literal ) -> type_ <> " " <> tokLexeme token <> " " <> literal
 
 
 scanTokens :: String -> Either [Error] [Token]
 scanTokens source =
-    Right [Token source]
+    Right [Token (IDENTIFIER source) 1 "carl"]
